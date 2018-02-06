a0a44e64f5c2bf1ed5b76a47fedc0835f8c7670b
eschos24 <eschos24@gmail.com>
2018-02-05 17:28:35 -0700

    Removed commented out code referencing CommandFactory singleton since we shouldn't need it

--- app/src/main/java/e/mboyd6/tickettoride/Communication/CommandFactory.java
+++ app/src/main/java/e/mboyd6/tickettoride/Communication/CommandFactory.java
@@ -8,21 +8,7 @@ import com.example.sharedcode.communication.Command;
 
 public class CommandFactory {
 
-    // ***** Uncomment if we actually need the singleton *****
-//    private static CommandFactory factory;
-//
-//    // Default initializer not accessible--must use singleton
-//    private CommandFactory(){};
-//
-//    public static CommandFactory instance() {
-//        if (factory == null) {
-//            factory = new CommandFactory();
-//        }
-//
-//        return factory;
-//    }
-
     public static Command createCommand(String className, String methodName, String[] paramTypesStringNames, Object[] paramValues) {
         return new Command(className, methodName, paramTypesStringNames, paramValues);
-    }
+    }   
 }
