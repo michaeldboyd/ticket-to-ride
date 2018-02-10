bd7009d527cfd6fb2cb179c6ec33f036cab16c06
Michael Boyd <michaeldavisboyd@gmail.com>
2018-02-10 15:03:59 -0700

    Ok,this is the last try.

--- app/src/main/java/e/mboyd6/tickettoride/Communication/CommandSocket.java
+++ app/src/main/java/e/mboyd6/tickettoride/Communication/CommandSocket.java
@@ -1,53 +1,53 @@
-package e.mboyd6.tickettoride.Communication;
-import com.example.sharedcode.communication.Command;
-import com.example.sharedcode.communication.CommandMessage;
-import com.google.gson.Gson;
-
-
-import org.eclipse.jetty.websocket.api.Session;
-import org.eclipse.jetty.websocket.api.annotations.WebSocket;
-
-import javax.websocket.ClientEndpoint;
-import javax.websocket.CloseReason;
-import javax.websocket.OnClose;
-import javax.websocket.OnError;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-
-@WebSocket
-@ClientEndpoint
-public class CommandSocket
-{
-    Gson gson = new Gson();
-    @OnOpen
-    public void onWebSocketConnect(Session sess)
-    {
-        System.out.println("Client Connected!");
-        // TODO: linke each session with the appropriate user. this is where it all starts
-    }
-
-
-
-    @OnMessage
-    public void onWebSocketText(String message)
-    {
-        Command res = gson.fromJson(message, Command.class);
-        try {
-            res.execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @OnClose
-    public void onWebSocketClose(CloseReason reason)
-    {
-        System.out.println("Socket Closed: " + reason);
-    }
-
-    @OnError
-    public void onWebSocketError(Throwable cause)
-    {
-        cause.printStackTrace(System.err);
-    }
-}
+//package e.mboyd6.tickettoride.Communication;
+//import com.example.sharedcode.communication.Command;
+//import com.example.sharedcode.communication.CommandMessage;
+//import com.google.gson.Gson;
+//
+//
+//import org.eclipse.jetty.websocket.api.Session;
+//import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+//
+//import javax.websocket.ClientEndpoint;
+//import javax.websocket.CloseReason;
+//import javax.websocket.OnClose;
+//import javax.websocket.OnError;
+//import javax.websocket.OnMessage;
+//import javax.websocket.OnOpen;
+//
+//@WebSocket
+//@ClientEndpoint
+//public class CommandSocket
+//{
+//    Gson gson = new Gson();
+//    @OnOpen
+//    public void onWebSocketConnect(Session sess)
+//    {
+//        System.out.println("Client Connected!");
+//        // TODO: linke each session with the appropriate user. this is where it all starts
+//    }
+//
+//
+//
+//    @OnMessage
+//    public void onWebSocketText(String message)
+//    {
+//        Command res = gson.fromJson(message, Command.class);
+//        try {
+//            res.execute();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @OnClose
+//    public void onWebSocketClose(CloseReason reason)
+//    {
+//        System.out.println("Socket Closed: " + reason);
+//    }
+//
+//    @OnError
+//    public void onWebSocketError(Throwable cause)
+//    {
+//        cause.printStackTrace(System.err);
+//    }
+//}
