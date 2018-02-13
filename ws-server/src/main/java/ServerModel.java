5ad04fe526dca6d367abe2b5d6113379bc95cc6c
eschos24 <eschos24@gmail.com>
2018-02-12 18:58:48 -0700

    Made loggedInSessions not a method and not static (ServerModel itself is a singleton)

--- ws-server/src/main/java/ServerModel.java
+++ ws-server/src/main/java/ServerModel.java
@@ -29,17 +29,9 @@ public class ServerModel {
     public Map<String, User> allUsers = new HashMap<>(); // <username, User>
     public Map<String, String> authTokenToUsername = new HashMap<>(); // <authToken, username>
     public Map<String, Game> games = new HashMap<>(); // <gameID, Game>
-    public Set<Session> sessionSet = new HashSet<Session>();
-    public Map<String, User> loggedInUsers = new HashMap<>();
-    public Map<String, User> allUsers = new HashMap<>();
-    public Map<String, Game> games = new HashMap<>();
-
-    public static Map<String, org.eclipse.jetty.websocket.api.Session> getLoggedInSessions() {
-        return loggedInSessions;
-    }
 
     //this static map keeps track of all open websockets with key: username val: session instance
-    public static Map<String, org.eclipse.jetty.websocket.api.Session> loggedInSessions = new HashMap<>();
+    public Map<String, org.eclipse.jetty.websocket.api.Session> loggedInSessions = new HashMap<>();
 
     public void setSession(org.eclipse.jetty.websocket.api.Session session) {
         this.session = session;
