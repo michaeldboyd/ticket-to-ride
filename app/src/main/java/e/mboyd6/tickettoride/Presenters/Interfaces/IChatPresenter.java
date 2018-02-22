df41c571f05c257fc171d5cf819c98dab53b92db
jonny <jonathanlinford@gmail.com>
2018-02-22 16:09:53 -0700

    start new branch

---
+++ app/src/main/java/e/mboyd6/tickettoride/Presenters/Interfaces/IChatPresenter.java
@@ -0,0 +1,15 @@
+package e.mboyd6.tickettoride.Presenters.Interfaces;
+
+/**
+ * Created by jonathanlinford on 2/20/18.
+ */
+
+public interface IChatPresenter {
+
+    // View > Presenter
+    void sendMessage(String message);
+
+    // Presenter > View
+    void messageReceived();
+    void isTypingChanged();
+}
