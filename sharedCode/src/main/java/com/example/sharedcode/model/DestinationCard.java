0e2ecbb761f1e39dd65379f9a9b06fa78987481a
alibub <alibub@gmail.com>
2018-02-24 20:59:10 -0700

    Added basic classes and begining functions for each to Model for Phase 2. I haven't seen our UML grade yet so I'm not sure if it's all good or not, so I'll wait to keep going till I know it is. :)

---
+++ sharedCode/src/main/java/com/example/sharedcode/model/DestinationCard.java
@@ -0,0 +1,15 @@
+package com.example.sharedcode.model;
+
+/**
+ * Created by Ali on 2/24/2018.
+ */
+
+public class DestinationCard {
+
+
+    //we might just want all of this to just have a route in it
+    private String startCity;
+    private String endCity; //enums?
+    private int points;
+
+}
---
+++ sharedCode/src/main/java/com/example/sharedcode/model/TrainCard.java
@@ -0,0 +1,15 @@
+package com.example.sharedcode.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ali on 2/24/2018.
+ */
+
+public class TrainCard  {
+
+    private String trainCarType;
+    private String color;
+
+
+}
