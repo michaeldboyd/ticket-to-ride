afabf8cf4e62600f9a8b9cdbfe87765d38587f72
alibub <alibub@gmail.com>
2018-02-26 16:34:30 -0700

    Added ChatMessage

---
+++ sharedCode/src/main/java/com/example/sharedcode/model/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.example.sharedcode.model;
+
+
+import java.sql.Timestamp;
+
+/**
+ * Created by Ali on 2/26/2018.
+ */
+
+public class ChatMessage {
+
+    private String message;
+    private String sender;
+    private Timestamp timestamp;
+
+}
