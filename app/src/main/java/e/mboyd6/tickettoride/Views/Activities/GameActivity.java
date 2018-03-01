b3f14e35cbb527c93975dc38e5791e58cb422320
htinney <hunter.tinney@gmail.com>
2018-02-28 21:17:33 -0700

    Designed GameActivity assets a little more

--- app/src/main/java/e/mboyd6/tickettoride/Views/Activities/GameActivity.java
+++ app/src/main/java/e/mboyd6/tickettoride/Views/Activities/GameActivity.java
@@ -92,8 +92,6 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
 
 
         // Set up the user interaction to manually show or hide the system UI.
@@ -107,7 +105,6 @@ public class GameActivity extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
