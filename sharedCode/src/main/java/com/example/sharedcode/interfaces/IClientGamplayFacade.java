fe5ab4bf206f2d5c4dfc2ca69e706cacd8bd2b0b
jonny <jonathanlinford@gmail.com>
2018-03-02 18:30:22 -0700

    More facade interfaces

--- sharedCode/src/main/java/com/example/sharedcode/interfaces/IClientGamplayFacade.java
+++ sharedCode/src/main/java/com/example/sharedcode/interfaces/IClientGamplayFacade.java
@@ -5,4 +5,11 @@ package com.example.sharedcode.interfaces;
  */
 
 public interface IClientGamplayFacade {
+    void claimedRoute(String gameID, String playerID);
+    void drewTrainCard(String gameID, String playerID);
+    void discardedTrainCard(String gameID, String playerID);
+    void drewDestinationCard(String gameID, String playerID);
+    void discardedDestinationCard(String gameID, String playerID);
+    void placedTrainCars(String gameID, String playerID);
+    void historyUpdated(String gameID, String[] s); //TODO: This string array was in the design, but I'm not sure what it is about
 }
