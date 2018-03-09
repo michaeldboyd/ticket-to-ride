f97485594beb7efbab88cee21f7da3577d709b96
jonny <jonathanlinford@gmail.com>
2018-03-09 00:24:06 -0700

    final changes for autorun

--- app/src/main/java/e/mboyd6/tickettoride/Model/Autoplayer.java
+++ app/src/main/java/e/mboyd6/tickettoride/Model/Autoplayer.java
@@ -101,32 +101,23 @@ public class Autoplayer {
                 presenter.onUpdateTurn();
 
                 stepText = "Step " + step + " - Changed player's turn to player " + game.getPlayers().get(1).getName() + ".";
-                step = -1;
                 break;
             case 6:
-                //drawing cards test
-
-                int count2 = 0;
-                while(model.getCurrentPlayer().getHand().get(TrainType.FREIGHT) < 5){
-                    count2 += 2;
-                    presenter.drawTrainCards(0, 1, 0);
-                }
-
-                stepText = "Step " + step + " - Drew an additional " + count2 + " cards from the deck";
-                break;
-            case 7:
                 //Claiming route test
-
-                Route route2 = new Route("Salt Lake City", "Park City", 1, TrainType.FREIGHT);
-
-                presenter.claimRoute(route2);
+                Route route2 = new Route("Salt Lake City", "Paradise", 4, TrainType.HOPPER);
+                game.getRoutesClaimed().put(route2, game.getPlayers().get(1));
+                game.getPlayers().get(1).getScore().setPoints(4);
+                game.getPlayers().get(1)
+                presenter.updateBoard();
 
                 stepText = "Step " + step + " - Claimed route from Salt Lake City to Park City\n" +
                         "Player score is now updated to " + score.getPoints() + " points.\n" +
                         "Number of train cards for player " + model.getCurrentPlayer().getName() + " is now" + score.getCards() + ".\n" +
                         "Number of train cards in deck is " + game.getTrainCardDeck().size() + ".\n" +
                         "Number of trains is now " + score.getTrains() + ".";
+                step = -1;
                 break;
+
         }
 
         Toast.makeText(context, stepText, Toast.LENGTH_LONG)
