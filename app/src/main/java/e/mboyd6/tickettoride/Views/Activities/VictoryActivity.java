55f78821b6bf0dbd0eb44247a217f88ec35ee37e
jonny <jonathanlinford@gmail.com>
2018-03-17 16:01:20 -0600

    Added Victory activity and resource files

---
+++ app/src/main/java/e/mboyd6/tickettoride/Views/Activities/VictoryActivity.java
@@ -0,0 +1,15 @@
+package e.mboyd6.tickettoride.Views.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import e.mboyd6.tickettoride.R;
+
+
+public class VictoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_victory);
+    }
+}
---
+++ app/src/main/java/e/mboyd6/tickettoride/Views/Interfaces/IVictoryActivity.java
@@ -0,0 +1,8 @@
+package e.mboyd6.tickettoride.Views.Interfaces;
+
+/**
+ * Created by jonathanlinford on 3/17/18.
+ */
+
+public interface IVictoryActivity {
+}
