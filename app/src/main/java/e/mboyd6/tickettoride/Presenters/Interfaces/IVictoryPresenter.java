3731d1fd7622b7e428e84d5a81bd47e64ff2fd8b
jonny <jonathanlinford@gmail.com>
2018-03-26 11:24:21 -0600

    Added get playersby score to victory presenter

--- app/src/main/java/e/mboyd6/tickettoride/Presenters/Interfaces/IVictoryPresenter.java
+++ app/src/main/java/e/mboyd6/tickettoride/Presenters/Interfaces/IVictoryPresenter.java
@@ -1,10 +1,16 @@
 package e.mboyd6.tickettoride.Presenters.Interfaces;
 
+import com.example.sharedcode.model.Player;
+
+import java.util.List;
+
 /**
  * Created by jonathanlinford on 3/17/18.
  */
 
 public interface IVictoryPresenter {
+    List<Player> getPlayersByScore();
+
     void returnToLobby();
     void loadScreen();
     void detachView();
--- app/src/main/java/e/mboyd6/tickettoride/Presenters/VictoryPresenter.java
+++ app/src/main/java/e/mboyd6/tickettoride/Presenters/VictoryPresenter.java
@@ -1,15 +1,17 @@
 package e.mboyd6.tickettoride.Presenters;
 
 import com.example.sharedcode.communication.UpdateArgs;
+import com.example.sharedcode.model.Game;
+import com.example.sharedcode.model.Player;
 
 import junit.framework.Assert;
 
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 import e.mboyd6.tickettoride.Model.ClientModel;
 import e.mboyd6.tickettoride.Presenters.Interfaces.IVictoryPresenter;
-import e.mboyd6.tickettoride.Views.Activities.VictoryActivity;
 import e.mboyd6.tickettoride.Views.Interfaces.IVictoryActivity;
 
 /**
@@ -19,11 +21,17 @@ import e.mboyd6.tickettoride.Views.Interfaces.IVictoryActivity;
 public class VictoryPresenter implements IVictoryPresenter, Observer {
 
     IVictoryActivity victoryActivity;
+    Game game = ClientModel.getInstance().getCurrentGame();
 
     public VictoryPresenter(IVictoryActivity victoryActivity) {
     //TODO: Implement the context setting
         ClientModel.getInstance().addObserver(this);
         this.victoryActivity = victoryActivity;
+    }
+
+    @Override
+    public List<Player> getPlayersByScore(){
+        return game.getPlayerListByScore();
     }
 
     @Override
