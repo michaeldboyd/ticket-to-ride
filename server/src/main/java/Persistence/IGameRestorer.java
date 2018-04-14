7de6f797d15267bf4484b9ec1c017de51f3f6f2b
jonny <jonathanlinford@gmail.com>
2018-04-14 15:44:53 -0600

    filling out the classes

--- server/src/main/java/Persistence/IGameRestorer.java
+++ server/src/main/java/Persistence/IGameRestorer.java
@@ -1,4 +1,18 @@
 package Persistence;
 
+import com.example.sharedcode.communication.Command;
+import com.example.sharedcode.model.Game;
+
+import java.util.List;
+
 public interface IGameRestorer {
+
+    void run();
+
+    void initFromDB();
+
+    List<Game> getGames();
+    List<Command> getCommandsForGame(String gameID);
+    void simulateGame(List<Command> commands);
+    void pushGameToServerModel(Game game) throws Exception;
 }
